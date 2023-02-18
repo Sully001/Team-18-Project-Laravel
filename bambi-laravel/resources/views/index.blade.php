8d708e387690c2e8e42aff0187742e13765dd3f7
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-02-18 22:21:28 +0000

    Added layout file to only have the header(navabar) and footer of the site which will be added here and all other files will extend this. Also added '/products' route to be developed.

---
+++ bambi-laravel/resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.layout')
+
+@section('content')
+    <h1>Welcome To Bambi Shoes Home Page. This is the products page i.e., ('/products') route</h1>
+    <p>test natalie.</p> 
+@endsection
