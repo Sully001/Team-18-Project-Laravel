94bc478058bd2bba8b841f5116c864713d92d130
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-02-18 23:34:04 +0000

    Deleted Laravel Files

--- bambi-laravel/resources/views/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.layout')
-
-@section('content')
-    <h1>Welcome To Bambi Shoes Home Page. This is the products page i.e., ('/products') route</h1>
-    <p>test natalie.</p> 
-@endsection
--- bambi-laravel/resources/views/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.layout')
-
-@section('content')
-    <h1>Welcome To Bambi Shoes Home Page. This is the home page i.e ('/') route</h1>
-    <p>test natalie.</p> 
-@endsection
