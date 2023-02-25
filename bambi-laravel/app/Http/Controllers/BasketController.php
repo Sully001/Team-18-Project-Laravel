5aa1bff98830c0416b38c4eedf57235307cf0ee3
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-02-25 17:49:29 +0000

    Added ability to view basket for logged in user

--- bambi-laravel/app/Http/Controllers/BasketController.php
+++ bambi-laravel/app/Http/Controllers/BasketController.php
@@ -3,20 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Basket;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class BasketController extends Controller
 {
     public function index($id) {
-        return view('basket');
+        $basket = Basket::where('user_id', '=', $id)->get();
+        return view('basket', [
+            'basket' => $basket,
+        ]);
     }
 
     public function store($id, Request $request) {
+        //Get the actual item
+        $product = Product::findorFail($request->product);
+
+
         $basket = new Basket();
         $basket->user_id = $id;
         $basket->product_id = $request->product;
+        $basket->product_name = $product->product_name;
         $basket->size = $request->size;
         $basket->quantity = $request->quantity;
+        $basket->price = $product->product_price;
+        $basket->product_image = $product->product_image;
 
         $basket->save();
 
--- bambi-laravel/resources/views/basket.blade.php
+++ bambi-laravel/resources/views/basket.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
-<h1>Basket Page</h1>
-<p>('/basket') route</p>    
+    <h1>Basket Page</h1>  
+
+    @foreach ($basket as $item)
+        <img src="/images/{{ $item->product_image}}" alt="Image" height="140px" width="140px">
+        <p>Product ID: {{ $item->product_id }}</p>
+        <p>Product Name: {{ $item->product_name }}</p>
+        <p>Shoe Size: {{ $item->size }}</p>
+        <p>Quantity: {{ $item->quantity }}</p>
+        <p>Price: £{{ $item->price }}</p>  
+    @endforeach
+
 @endsection
