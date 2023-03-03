e9f5a0d03bfbdaa1de6bd88c917c2afa1672bfff
SedulousdFlame <t.gilkes217@gmail.com>
2023-03-03 01:16:46 +0000

    Updated and changed forms in login.blade.php

--- bambi-laravel/resources/views/login.blade.php
+++ bambi-laravel/resources/views/login.blade.php
@@ -11,62 +11,80 @@
         {{ session('mssg') }}
     @endif
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 
-    <h1>Register Section</h1>
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
-        <div>
-            <label for="first_name">First Name:</label>
-            <input type="text" name="first_name" id="first_name" placeholder="First Name" value="{{ old('first_name') }}">
-        </div>
-        @error('first_name')
-            <p>{{ $message }}</p>
-        @enderror
-        <div>
-            <label for="last_name">Last Name:</label>
-            <input type="text" name="last_name" id="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
-        </div>
-        @error('last_name')
-            <p>{{ $message }}</p>
-        @enderror
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="youremail@example.com" value="{{ old('email') }}">
-        </div>
-        @error('email')
-            <p>{{ $message }}</p>
-        @enderror
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password">
-        </div>
-        <div>
-            <label for="password_confirmation">Repeat Password</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Password">
-        </div>
-        @error('password')
-            <p>{{ $message }}</p>
-        @enderror
+<div class = "page">
+    <div id = "log_reg">
+        <div class = "form_sect" id = "reg_form">
+            <div class = "header_caption">
+                <h2 class = "header" id = "register_header">Register</h2>
+                <p class = "caption" id = "register_header">Sign up today, it’s free</p>
+            </div>
+            <form class = "forms_" action="{{ route('register') }}" method="POST">
+                @csrf
+                <div>
+                    <!--<label for="first_name">First Name:</label>-->
+                    <input class = "input_fields" type="text" name="first_name" id="first_name" placeholder="First Name" value="{{ old('first_name') }}">
+                </div>
+                @error('first_name')
+                    <p>{{ $message }}</p>
+                @enderror
+                <div>
+                    <!--<label for="last_name">Last Name:</label>-->
+                    <input class = "input_fields" type="text" name="last_name" id="last_name" placeholder="Last Name" value="{{ old('last_name') }}">
+                </div>
+                @error('last_name')
+                    <p>{{ $message }}</p>
+                @enderror
+                <div>
+                    <!--<label for="email">Email</label>-->
+                    <input class = "input_fields"  type="email" name="email" id="email" placeholder="Email address" value="{{ old('email') }}">
+                </div>
+                @error('email')
+                    <p>{{ $message }}</p>
+                @enderror
+                <div>
+                    <!--<label for="password">Password</label>-->
+                    <input class = "input_fields"  type="password" name="password" id="password" placeholder="Password">
+                </div>
+                <div>
+                    <!--<label for="password_confirmation">Repeat Password</label>-->
+                    <input class = "input_fields" type="password" name="password_confirmation" id="password_confirmation" placeholder="Password">
+                </div>
+                @error('password')
+                    <p>{{ $message }}</p>
+                @enderror
 
-        <button type="submit">Register</button>
-    </form>
-
-    <h1>Login Section</h1>
-
-    @if (session('status'))
-        {{ session('status') }}
-    @endif
-
-    <form action="{{ route('signin') }}" method="POST">
-        @csrf
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="youremail@example.com" value="{{ old('email') }}">
+                <button id = "reg_submit" class = "submit_button" type="submit">Register</button>
+            </form>
         </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password">
+
+        <div class = "form_sect">
+        <div class = "header_caption">
+            <h2 class = "header" id = "login_header">Login</h2>
+            <p class = "caption" id = "register_header">Sign up today, it’s free</p>
         </div>
-        <button type="submit">Login</button>
-    </form>
+
+            @if (session('status'))
+                {{ session('status') }}
+            @endif
+
+            <form class = "forms_" action="{{ route('signin') }}" method="POST">
+                @csrf
+                <div>
+                    <!--<label for="email">Email</label>-->
+                    <input class = "input_fields"  type="email" name="email" id="email" placeholder="Email address" value="{{ old('email') }}">
+                </div>
+                <div>
+                    <!--<label for="password">Password</label>-->
+                    <input class = "input_fields"  type="password" name="password" id="password" placeholder="Password">
+                </div>
+                <button id = "log_submit" class = "submit_button" type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
