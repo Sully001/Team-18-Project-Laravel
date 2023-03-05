d7d62d1dbea45fbcb023d4ecb232d7e9f5d21f04
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-03-05 17:59:22 +0000

    Updated basket view to loop through the data returned by the basket controller

--- bambi-laravel/resources/views/basket.blade.php
+++ bambi-laravel/resources/views/basket.blade.php
@@ -15,40 +15,34 @@
       <p>{{ session('delete') }}</p>  
   @endif
 
-  <form action="{{ route('checkout')}}" method="POST">
-    @csrf
+  <p>Subtotal: £{{ $total }}</p>
+    
+  <p>Total: £{{ $total }}</p>
+
+  <form action="{{ route('checkout')}}" method="POST"> 
+    @csrf    
+    <input type="hidden" name="total" value="{{ $total }}">
     <button type="submit">Checkout</button>
   </form>
-   
+  <br>
 
-  <p>Subtotal: 
-    @php
-      $subtotal = 0;
-      foreach ($basket as $item) {
-      $sum = $item->price * $item->quantity;
-      $subtotal += $sum;
-      }
-      echo '£'.$subtotal;
-    @endphp
-  </p>
-  <p>Total: @php echo '£'.$subtotal @endphp</p>
+  @foreach ($products as $product)
+    <img src="/images/{{ $product['image'] }}" alt="Shoes" height="140px" width="140px">
+      <p>Product ID: {{ $product['id'] }}</p>
+      <p>Product ID: {{ $product['brand'] }}</p>
+      <p>Product Name: {{ $product['name'] }}</p>
+      <p>Price: £{{ $product['price'] }}</p>  
+      <p>Shoe Size: {{ $product['size'] }}</p>
+      <p>Quantity: {{ $product['quantity'] }}</p>
 
-
-  @foreach ($basket as $item)
-      <img src="/images/{{ $item->product_image}}" alt="Image" height="140px" width="140px">
-      <p>Product ID: {{ $item->product_id }}</p>
-      <p>Product Name: {{ $item->product_name }}</p>
-      <p>Shoe Size: {{ $item->size }}</p>
-      <p>Quantity: {{ $item->quantity }}</p>
-      <p>Price: £{{ $item->price }}</p>  
-
-      <form action="{{ route('basket.remove') }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <input type="hidden" name="id" value="{{ $item->product_id }}">
-        <input type="hidden" name="size" value="{{ $item->size }}">
-        <button class="btn btn-danger">Remove</button>
+    <form action="{{ route('basket.remove') }}" method="POST">
+      @csrf
+      @method('DELETE')
+      <input type="hidden" name="id" value="{{ $product['id'] }}">
+      <input type="hidden" name="size" value="{{ $product['size'] }}">
+      <button class="btn btn-danger">Remove</button>
     </form>
+    <br>
   @endforeach
 
 @endsection
