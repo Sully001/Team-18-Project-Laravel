a5eb3490aac66de418884d926b74ba7fd2c56ba3
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2023-03-21 10:44:50 +0000

    minor change

--- bambi-laravel/resources/views/orders.blade.php
+++ bambi-laravel/resources/views/orders.blade.php
@@ -7,36 +7,30 @@
 @section('content')
 
   @foreach ($orders as $order)
-    <!-- <p>Order Number: {{ $loop->index + 1 }}
-       | Total: {{ $order->total }} 
-       | Order Status: {{ $order->order_completion }} 
-       | Order Date & Time: {{ $order->created_at }}
-       <a href="{{ route('previous.orders',$order->order_id) }}">View Order</a>
-    </p> -->
+ 
+  <div class="orders-contain">
 
-<div class="orders-contain">
+    <div class="orders-card">
+      <div class="order-no"> 
+        <p>Order No: {{ $loop->index + 1 }}</p>
+      </div>
+      <div class="order-status"> 
+        <p>Status: {{ $order->order_completion }}</p> 
+      </div>
+      <div class="order-dt"> 
+        <p>{{ $order->created_at }}</p>
+      </div>
+      <div class="order-total"> 
+        <p>£{{ $order->total }}</p> 
+      </div>
+      <div class="view-order"> 
+        <p><a href="{{ route('previous.orders',$order->order_id) }}">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </a></p>
+      </div>
+    </div>
 
-  <div class="orders-card">
-    <div class="order-no"> 
-      <p>Order No: {{ $loop->index + 1 }}</p>
-    </div>
-    <div class="order-status"> 
-      <p>Status: {{ $order->order_completion }}</p> 
-    </div>
-    <div class="order-dt"> 
-      <p>{{ $order->created_at }}</p>
-    </div>
-    <div class="order-total"> 
-      <p>£{{ $order->total }}</p> 
-    </div>
-    <div class="view-order"> 
-      <p><a href="{{ route('previous.orders',$order->order_id) }}">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      </a></p>
-    </div>
   </div>
-
-</div>
 
 @endforeach
 
