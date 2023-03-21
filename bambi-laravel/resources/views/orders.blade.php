9f799b3606477fee40d6ce45eca63683d80ae99c
Sadiyahhh <116123629+Sadiyahhh@users.noreply.github.com>
2023-03-21 00:11:17 +0000

    orders page + styling

--- bambi-laravel/resources/views/orders.blade.php
+++ bambi-laravel/resources/views/orders.blade.php
@@ -1,18 +1,43 @@
 @extends('layouts.layout')
 
 @section('css')
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="/css/orders.css">
 @endsection
 
 @section('content')
-  <h1>Your Previous orders</h1> 
+
   @foreach ($orders as $order)
-    <p>Order Number: {{ $loop->index + 1 }}
+    <!-- <p>Order Number: {{ $loop->index + 1 }}
        | Total: {{ $order->total }} 
        | Order Status: {{ $order->order_completion }} 
        | Order Date & Time: {{ $order->created_at }}
        <a href="{{ route('previous.orders',$order->order_id) }}">View Order</a>
-    </p>
-  @endforeach
+    </p> -->
+
+<div class="orders-contain">
+
+  <div class="orders-card">
+    <div class="order-no"> 
+      <p>Order No: {{ $loop->index + 1 }}</p>
+    </div>
+    <div class="order-status"> 
+      <p>Status: {{ $order->order_completion }}</p> 
+    </div>
+    <div class="order-dt"> 
+      <p>{{ $order->created_at }}</p>
+    </div>
+    <div class="order-total"> 
+      <p>£{{ $order->total }}</p> 
+    </div>
+    <div class="view-order"> 
+      <p><a href="{{ route('previous.orders',$order->order_id) }}">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      </a></p>
+    </div>
+  </div>
+
+</div>
+
+@endforeach
 
 @endsection
