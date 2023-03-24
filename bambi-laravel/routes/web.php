55dfdeca13c1a7c4ba1722d58d524dccb84048f5
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-03-24 01:09:10 +0000

    Added middleware to routes and added reject GET routes

--- bambi-laravel/routes/web.php
+++ bambi-laravel/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Basket;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
@@ -23,29 +22,42 @@ use App\Http\Controllers\RegisterController;
 |
 */
 // lots of routes that require auth middleware
+Route::group(['middleware' => 'auth'], function () {
+    //POST logout
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-//POST logout
-Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+    //Basket routes
+    Route::delete('/basket/remove', [BasketController::class, 'destroy'])->name('basket.remove');
+    Route::get('/basket/{id}', [BasketController::class, 'index'])->name('basket')->middleware(['checkId']);
+    Route::post('/basket', [BasketController::class, 'store'])->name('basket.store');
 
-//Basket routes
-Route::delete('/basket/remove', [BasketController::class, 'destroy'])->name('basket.remove');
-Route::get('/basket/{id}', [BasketController::class, 'index'])->name('basket');
-Route::post('/basket', [BasketController::class, 'store'])->name('basket.store');
+    //Order routes
+    Route::post('/order', [OrderController::class, 'checkout'])->name('checkout');
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders');
 
-//Order routes
-Route::post('/order', [OrderController::class, 'checkout'])->name('checkout');
-Route::get('/orders', [OrderController::class, 'index'])->name('orders');
+    //Order Detail Routes
+    Route::get('/order/{id}', [PreviousOrderController::class, 'index'])->name('previous.orders')->middleware(['checkId']);
 
-//Order Detail Routes
-Route::get('/order/{id}', [PreviousOrderController::class, 'index'])->name('previous.orders');
+    //Email Route for Register and Order Email Notif
+    Route::get('/mail', [MailController::class, 'sendMail']);
+});
+
+Route::group(['middleware' => 'guest'], function () { 
+    //Gets login page
+    Route::get('/loginpage', [RegisterController::class, 'index'])->name('login');
+    //Register route
+    Route::post('/register', [RegisterController::class, 'store'])->name('register');
+    //Login route
+    Route::post('/login', [LoginController::class, 'store'])->name('signin');
+});
 
 
-//Login route
-Route::post('login', [LoginController::class, 'store'])->name('signin');
-//Gets login page
-Route::get('/loginpage', [RegisterController::class, 'index'])->name('login');
-//Register route
-Route::post('/register', [RegisterController::class, 'store'])->name('register');
+Route::group(['middleware' => 'reject'], function () {
+    Route::get('/logout', [LogoutController::class, 'logout']);
+    Route::get('/basket', [BasketController::class, 'store']);
+    Route::get('/order', [OrderController::class, 'checkout']);
+
+});
 
 
 //Global Routes
@@ -70,6 +82,5 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/mail', [MailController::class, 'sendMail']);
 Route::post('/contact/email', [HomeController::class, 'contactUs'])->name('contact.email');
 
