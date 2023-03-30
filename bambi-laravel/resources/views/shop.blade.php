4d841cfa14aa35ee897d804bd05d1920f400d533
Atalyah Frost <210182725@aston.ac.uk>
2023-03-30 16:35:43 +0100

    cleaned up code

--- bambi-laravel/resources/views/shop.blade.php
+++ bambi-laravel/resources/views/shop.blade.php
@@ -13,30 +13,27 @@
       
     <div class="shop screen">
 
-      <div class="hero">
+      <div class="shop-header">
         <div class="flex-col  flex">
-          <p class="receive-and-extra-sp montserrat-medium-black-32px">
-            <span class="montserrat-medium-black-32px">Receive and extra special </span><span class="span1">free
-              gift</span><span class="montserrat-medium-black-32px"> with your luxury shoes!</span>
+          <p class="header-promo-title header-text">
+            <span class="header-text">Receive and extra special </span><span class="span1">free
+              gift</span><span class="header-text"> with your luxury shoes!</span>
           </p>
-          <p class="lorem-ipsum-dolor-si">
+          <p class="header-promo">
             With shoe purchases £100 or more, choose between free Dubbin waterproofing or any accessory worth up to £20.
             And with shoe purchases between £100 and £500, you’ll receive lifetime warranty on us. Just consider it a
             gift from us to you.
           </p>
         </div>
-        <img class="rectangle-15" src="/Website-Images/rectangle-15.png" alt="Rectangle 15" />
+        <img class="header-img" src="/Website-Images/rectangle-15.png" alt="Header Image" />
       </div>
       
-      
-      
-      
-      <div class="overlap-group2">
+      <div class="main">
         
         <div class="search">
           
           <div class="overlap-group">
-            <div class="search-1 valign-text-middle montserrat-normal-black-24px">
+            <div class="search-1 body-text">
             <form action="{{route('search')}}" class="search-bar">
                 <input type="Search" name="search" id="search" placeholder="Search">
                 <!-- <button type="submit"><img src="/Website-Images/magnifying-glass-icon.png"></button> -->
@@ -44,7 +41,6 @@
             </div>
             <div class="overlap-group1">
               <button type="submit"><img src="/Website-Images/magnifying-glass-icon.png"></button>
-              <!-- <img class="icon-search" src="/Website-Images/magnifying-glass-icon.png" alt="icon-search" /> -->
             </form>
             </div>
           </div>
@@ -55,45 +51,40 @@
             <a class="link" href="{{ URL::current()."?sort=asc" }}"><div>Low to High</div></a>
           </div>
 
-          <div class="women montserrat-bold-black-24px">WOMEN</div>
-          <a href="{{route('products.women')}}"><div class="all-womens-shoes montserrat-bold-black-24px">All womens shoes</div></a>
-          <a href="{{route('women.trainers')}}"><div class="trainers montserrat-normal-black-24px">Trainers</div></a>
-          <a href="{{route('women.boots')}}"><div class="search-item montserrat-normal-black-24px">Boots</div></a>
-          <a href="{{route('women.heels')}}"><div class="search-item montserrat-normal-black-24px">Heels</div></a>
-          <a href="{{route('women.flats')}}"><div class="search-item montserrat-normal-black-24px">Flats</div></a>
-          <a href="{{route('women.sandals')}}"><div class="search-item montserrat-normal-black-24px">Sandals</div></a>
+          <div class="women">WOMEN</div>
+          <a href="{{route('products.women')}}"><div class="all-womens-shoes body-text">All womens shoes</div></a>
+          <a href="{{route('women.trainers')}}"><div class="trainers body-text">Trainers</div></a>
+          <a href="{{route('women.boots')}}"><div class="search-prods body-text">Boots</div></a>
+          <a href="{{route('women.heels')}}"><div class="search-prods body-text">Heels</div></a>
+          <a href="{{route('women.flats')}}"><div class="search-prods body-text">Flats</div></a>
+          <a href="{{route('women.sandals')}}"><div class="search-prods body-text">Sandals</div></a>
           
-          <div class="men montserrat-bold-black-24px">MEN</div>
-          <a href="{{route('products.men')}}"><div class="all-mens-shoes montserrat-bold-black-24px">All mens shoes</div></a>
-          <a href="{{route('men.trainers')}}"><div class="search-item montserrat-normal-black-24px">Trainers</div></a>
-          <a href="{{route('men.loafers')}}"><div class="search-item montserrat-normal-black-24px">Loafers</div></a>
-          <a href="{{route('men.boots')}}"><div class="search-item montserrat-normal-black-24px">Boots</div></a>
-          <a href="{{ route('men.dress')}}"><div class="search-item montserrat-normal-black-24px">Dress Shoes</div></a>
-          <a href="{{ route('men.sliders')}}"><div class="search-item montserrat-normal-black-24px">Sliders</div></a>
+          <div class="men">MEN</div>
+          <a href="{{route('products.men')}}"><div class="all-mens-shoes body-text">All mens shoes</div></a>
+          <a href="{{route('men.trainers')}}"><div class="search-prods body-text">Trainers</div></a>
+          <a href="{{route('men.loafers')}}"><div class="search-prods body-text">Loafers</div></a>
+          <a href="{{route('men.boots')}}"><div class="search-prods body-text">Boots</div></a>
+          <a href="{{ route('men.dress')}}"><div class="search-prods body-text">Dress Shoes</div></a>
+          <a href="{{ route('men.sliders')}}"><div class="search-prods body-text">Sliders</div></a>
         </div>
 
-
-
-        <div class="items-1 items-2">
+        <div class="products-overlay-1 products-overlay-2">
           <a href="{{ route('products.index')}}"><h1 class="title">Shop All Shoes</h1></a>
-          
 
-          <!-- <div class="flex-row flex"> -->
-          
-          <div class="prod_box flex">
+          <div class="prod-box flex">
 
             @foreach ($products as $product)
 
-              <article class="items items-2 grid-item">
+              <article class="products products-overlay-2 grid-item">
 
-                <div class="rectangle"><img src="/images/{{ $product->product_image}}" alt="Shoes" ></div>
-                <div class="place valign-text-middle montserrat-semi-bold-black-20px">{{ $product->product_brand }}</div>
-                <div class="model valign-text-middle montserrat-normal-black-15px">{{ $product->product_name }}</div>
-                <div class="surname valign-text-middle montserrat-semi-bold-black-15px">£{{ $product->product_price }}</div>
+                <div class="prod-img"><img src="/images/{{ $product->product_image}}" alt="Shoes" ></div>
+                <div class="prod-brand">{{ $product->product_brand }}</div>
+                <div class="prod-name">{{ $product->product_name }}</div>
+                <div class="prod-price">£{{ $product->product_price }}</div>
 
                 <a href="{{ route('products.show', $product->product_id) }}">
                 <div class="button button-3">
-                  <div class="log-in-1 valign-text-middle montserrat-bold-white-15px">View Item</div>
+                  <div class="view-item">View Item</div>
                 </div>
                 </a>
 
@@ -107,8 +98,8 @@
         </div>
       </div>
 
-      <div class="credit-hero">
-        <p class="shop-shoes-from-14">
+      <div class="shop-footer">
+        <p class="footer-text">
         Spread the cost with up to 4 years interest free credit*
         <br></br>
         Subject to T&amp;Cs
@@ -116,29 +107,6 @@
           Shop shoes from £14.07 per month*. 0% finance is available on all purchases over £99.
         </p>
       </div>
-    
-      <!-- <div class="footer-a">
-        <div class="flex-row-1 montserrat-bold-alpine-15px">
-          <img class="transparent-logo1.png" src="/Website-Images/transparent-logo2-large.png" alt="transparent logo" />
-          <div class="shop-1">Shop</div>
-          <div class="t-us">About Us</div>
-          <div class="t-us">Contact Us</div>
-          <div class="button-2 button-3">
-            <div class="log-in valign-text-middle montserrat-bold-white-15px">Log in</div>
-          </div>
-        </div>
-        <div class="rectangle-1"></div>
-        
-        <div class="flex-row-2 montserrat-normal-black-15px-2">
-          <p class="copyright">© Bambi Shoes LTD, 2022</p>
-          <div class="follow-us">Follow us:</div>
-          <img class="ellipse-1 ellipse-3" src="/Website-Images/ellipse.svg" alt="Ellipse" />
-          <img class="ellipse-2 ellipse-3" src="/Website-Images/ellipse.svg" alt="Ellipse" />
-          <img class="ellipse ellipse-3" src="/Website-Images/ellipse.svg" alt="Ellipse" />
-          <img class="ellipse ellipse-3" src="/Website-Images/ellipse.svg" alt="Ellipse" />
-        </div>
-
-      </div> -->
     </div>
   </div>
 @endsection
